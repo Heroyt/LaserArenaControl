372d09fa8934fa27d4b690d1e487055b598fecc1
Tomáš Vojík <vojik@wboy.cz>
2021-11-08 16:34:26 +0100

    :construction: Laser gates controller script

---
+++ src/Tools/GatesController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Tools;
+
+use Exception;
+
+class GatesController
+{
+
+	public const PORT          = 666;
+	public const START_COMMAND = '0105010080';
+	public const END_COMMAND   = '0105020080';
+
+	/**
+	 * @param string $ip
+	 *
+	 * @return int
+	 * @throws Exception
+	 */
+	public static function start(string $ip) : int {
+		return self::sendCommand($ip, hex2bin(self::START_COMMAND));
+	}
+
+	/**
+	 * @param string $ip
+	 * @param string $command
+	 *
+	 * @return int
+	 * @throws Exception
+	 */
+	public static function sendCommand(string $ip, string $command) : int {
+		$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+		if ($sock === false) {
+			throw new Exception(lang('Nepodařilo se vytvořit socket.'));
+		}
+		$res = socket_connect($sock, $ip, self::PORT);
+		if ($res === false) {
+			throw new Exception(lang('Nepodařilo se připojit k socket serveru ('.$ip.':'.self::PORT.').'));
+		}
+		$a = socket_write($sock, $command, 5);
+		socket_close($sock);
+		return $a;
+	}
+
+	/**
+	 * @param string $ip
+	 *
+	 * @return int
+	 * @throws Exception
+	 */
+	public static function end(string $ip) : int {
+		return self::sendCommand($ip, hex2bin(self::END_COMMAND));
+	}
+
+}

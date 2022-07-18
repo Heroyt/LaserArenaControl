f349c1a827031b2b5968454f72f58d9d0f4efd80
Tomáš Vojík <vojik@wboy.cz>
2022-07-18 09:39:11 +0200

    :green_heart: Try to fix CI test suite #9

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,10 @@ use App\Install\DbInstall;
 define('ROOT', dirname(__DIR__).'/');
 const INDEX = true;
 
+if (!file_exists(ROOT.'private/config.ini')) {
+	copy(ROOT.'tests/private/config.ini', ROOT.'private/config.ini');
+}
+
 /*$noDb = ['--testsuite=unit', '--testsuite="unit"'];
 
 if (isset($GLOBALS['argv'][1]) && in_array($GLOBALS['argv'][1], $noDb, true)) {

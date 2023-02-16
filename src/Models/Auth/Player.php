fba90086ec7ad83f042227b2ad086f9da31fcf9d
Tomáš Vojík <vojik@wboy.cz>
2023-02-16 16:03:32 +0100

    :sparkles: Extended user player class

--- src/Models/Auth/Player.php
+++ src/Models/Auth/Player.php
@@ -3,8 +3,10 @@
 namespace App\Models\Auth;
 
 use App\Models\Auth\Validators\PlayerCode;
+use InvalidArgumentException;
 use Lsr\Core\DB;
 use Lsr\Core\Exceptions\ValidationException;
+use Lsr\Core\Models\Attributes\OneToMany;
 use Lsr\Core\Models\Attributes\PrimaryKey;
 use Lsr\Core\Models\Attributes\Validation\Email;
 use Lsr\Core\Models\Model;
@@ -21,6 +23,20 @@ class Player extends Model
 	public string $nickname;
 	#[Email]
 	public string $email;
+
+	/** @var PlayerConnection[] */
+	#[OneToMany(class: PlayerConnection::class)]
+	public array $connections = [];
+
+	public int $rank = 100;
+
+	public static function getByCode(string $code) : ?static {
+		$code = strtoupper(trim($code));
+		if (preg_match('/(\d)+-([A-Z\d]{5})/', $code) !== 1) {
+			throw new InvalidArgumentException('Code is not valid');
+		}
+		return static::query()->where('[code] = %s', $code)->first();
+	}
 
 	/**
 	 * @param string $code
@@ -52,5 +68,49 @@ class Player extends Model
 	 */
 	public function getCode() : string {
 		return $this->code;
+	}
+
+	/**
+	 * @return PlayerConnection[]
+	 * @throws ValidationException
+	 */
+	public function getConnections() : array {
+		if (empty($this->connections)) {
+			$this->connections = PlayerConnection::getForPlayer($this);
+		}
+		return $this->connections;
+	}
+
+	public function addConnection(PlayerConnection $connection) : Player {
+		// Find duplicates
+		$found = false;
+		foreach ($this->getConnections() as $connectionToTest) {
+			if ($connectionToTest->type === $connection->type && $connection->identifier === $connectionToTest->identifier) {
+				$found = true;
+				break;
+			}
+		}
+		if (!$found) {
+			$this->connections[] = $connection;
+		}
+		return $this;
+	}
+
+	public function jsonSerialize() : array {
+		$connections = [];
+		try {
+			foreach ($this->getConnections() as $connection) {
+				$connections[] = ['type' => $connection->type->value, 'identifier' => $connection->identifier];
+			}
+		} catch (ValidationException) {
+		}
+		return [
+			'id'          => $this->id,
+			'nickname'    => $this->nickname,
+			'code'        => $this->getCode(),
+			'email'       => $this->email,
+			'rank'        => $this->rank,
+			'connections' => $connections,
+		];
 	}
 }

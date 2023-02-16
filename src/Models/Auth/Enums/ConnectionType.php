94cc55633d150e57a3e97b1fe7ce1c7e842e7cfa
Tomáš Vojík <vojik@wboy.cz>
2023-02-16 16:03:51 +0100

    :sparkles: New default ConnectionType value

--- src/Models/Auth/Enums/ConnectionType.php
+++ src/Models/Auth/Enums/ConnectionType.php
@@ -2,10 +2,17 @@
 
 namespace App\Models\Auth\Enums;
 
+/**
+ * @property string $value
+ * @method static ConnectionType from(string $type)
+ * @method static ConnectionType|null tryFrom(string $type)
+ */
 enum ConnectionType: string
 {
 
 	case RFID = 'rfid';
 	case LASER_FORCE = 'laserforce';
+
+	case OTHER = 'other';
 
 }

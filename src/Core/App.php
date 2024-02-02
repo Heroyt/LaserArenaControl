23c333b52f47af25096c976a0668ec6ed07e32a5
Tomáš Vojík <vojik@wboy.cz>
2024-02-02 01:35:03 +0100

    :poop: Menu caching considers language

--- src/Core/App.php
+++ src/Core/App.php
@@ -11,7 +11,7 @@ class App extends \Lsr\Core\App
 	public static function getMenu(string $type = 'menu'): array {
 		return self::getServiceByType(Cache::class)
 		           ->load(
-			           'menu.' . $type . '.items',
+			           'menu.' . $type . '.items.' . self::getShortLanguageCode(),
 			           fn() => self::getServiceByType(MenuBuilder::class)->getMenu($type),
 			           [
 				           Cache::Tags => ['core', 'core.menu'],

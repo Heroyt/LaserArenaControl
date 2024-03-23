83abae0a3d462ff7b750c971cd22ac29f5ec7b53
Tomáš Vojík <vojik@wboy.cz>
2024-03-23 17:42:36 +0100

    :sparkles: Added API helpers
    - Added new data objects
    - Prepared for swagger documentation

---
+++ src/Api/OpenApi.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Api;
+
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+	version    : '1.0',
+	description: 'All API methods on the Laser arena Control app.',
+	title      : 'Laser arena Control API',
+)]
+#[OA\Server(url: 'https://lac.local')]
+class OpenApi
+{
+
+}

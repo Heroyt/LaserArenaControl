9da7fcbb8af212756169bbe074f6426d552e0a86
Tomáš Vojík <vojik@wboy.cz>
2024-04-09 19:23:00 +0200

    :sparkles: Gender and inflection services

---
+++ src/Helpers/Gender.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Helpers;
+
+use OpenApi\Attributes as OA;
+
+/**
+ * @property string $value
+ */
+#[OA\Schema(type: 'string')]
+enum Gender : string
+{
+
+	case MALE = 'm';
+	case FEMALE = 'f';
+	case OTHER = 'o';
+
+}

132c718763b1269281256f0e6b39b500920dd4ed
Tomáš Vojík <vojik@wboy.cz>
2024-04-10 18:22:00 +0200

    :construction: Added better error reporting for roadrunner

--- psr-worker.php
+++ psr-worker.php
@@ -1,13 +1,16 @@
 <?php
 
+use App\Api\Response\ErrorDto;
+use App\Api\Response\ErrorType;
 use App\Core\App;
 use App\Tasks\TaskDispatcherInterface;
-use Lsr\Core\Requests\Request;
+use Lsr\Core\Requests\Exceptions\RouteNotFoundException;
 use Lsr\Core\Requests\RequestFactory;
 use Lsr\Interfaces\SessionInterface;
 use Lsr\Logging\Logger;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
 use Spiral\RoadRunner\Environment;
 use Spiral\RoadRunner\Environment\Mode;
 use Spiral\RoadRunner\Http\PSR7Worker;
@@ -30,88 +33,128 @@ $env = Environment::fromGlobals();
 $isJobsMode = $env->getMode() === Mode::MODE_JOBS;
 
 if ($isJobsMode) {
-	$consumer = new Consumer();
-	/** @var Spiral\RoadRunner\Jobs\Task\ReceivedTaskInterface $task */
-	while ($task = $consumer->waitTask()) {
-		try {
-			$name = $task->getName();
+    $consumer = new Consumer();
+    /** @var Spiral\RoadRunner\Jobs\Task\ReceivedTaskInterface $task */
+    while ($task = $consumer->waitTask()) {
+        try {
+            $name = $task->getName();
 
-			/** @var TaskDispatcherInterface $dispatcher */
-			$dispatcher = App::getService($name);
-			$dispatcher->process($task);
+            /** @var TaskDispatcherInterface $dispatcher */
+            $dispatcher = App::getService($name);
+            $dispatcher->process($task);
 
-			if (!$task->isCompleted()) {
-				$task->complete();
-			}
-		} catch (Throwable $e) {
-			$task->fail($e);
-		}
-	}
+            if (!$task->isCompleted()) {
+                $task->complete();
+            }
+        } catch (Throwable $e) {
+            $task->fail($e);
+        }
+    }
 }
 else {
-	$worker = Worker::create();
+    $worker = Worker::create();
 
-	$logger = new Logger(LOG_DIR, 'worker');
-	$factory = new Psr17Factory();
-	$psr7 = new PSR7Worker($worker, $factory, $factory, $factory);
+    $logger = new Logger(LOG_DIR, 'worker');
+    $factory = new Psr17Factory();
+    $psr7 = new PSR7Worker($worker, $factory, $factory, $factory);
 
-	while (true) {
-		try {
-			$request = $psr7->waitRequest();
-			if ($request === null) {
-				break;
-			}
-		} catch (Throwable $e) {
-			// Although the PSR-17 specification clearly states that there can be
-			// no exceptions when creating a request, however, some implementations
-			// may violate this rule. Therefore, it is recommended to process the
-			// incoming request for errors.
-			//
-			// Send "Bad Request" response.
-			$psr7->respond(new Response(400));
-			continue;
-		}
+    while (true) {
+        try {
+            $request = $psr7->waitRequest();
+            if ($request === null) {
+                break;
+            }
+        } catch (Throwable $e) {
+            // Although the PSR-17 specification clearly states that there can be
+            // no exceptions when creating a request, however, some implementations
+            // may violate this rule. Therefore, it is recommended to process the
+            // incoming request for errors.
+            //
+            // Send "Bad Request" response.
+            $psr7->respond(new Response(400));
+            continue;
+        }
 
-		try {
-			$request = RequestFactory::fromPsrRequest($request);
-			App::setRequest($request);
+        try {
+            $request = RequestFactory::fromPsrRequest($request);
+            App::setRequest($request);
 
-			var_dump('Request: '.$request->getUri());
-			var_dump('App: '.App::getRequest()->getUri());
+            var_dump('Request: '.$request->getUri());
+            var_dump('App: '.App::getRequest()->getUri());
 
-			/** @var SessionInterface $session */
-			$session = App::getService('session');
-			if (!$session->isInitialized()) {
-				$session->init();
-			}
+            /** @var SessionInterface $session */
+            $session = App::getService('session');
+            if (!$session->isInitialized()) {
+                $session->init();
+            }
 
-			Debugger::$usePsr7 = true;
-			//Debugger::enable(PRODUCTION ? Debugger::Production : Debugger::Development, LOG_DIR);
+            Debugger::$usePsr7 = true;
+            //Debugger::enable(PRODUCTION ? Debugger::Production : Debugger::Development, LOG_DIR);
 
-			/*$get = $request->getQueryParams();
-			if (isset($get['_tracy_bar'])) {
-				$_GET = $get;
-				ob_start();
-				Debugger::dispatchPsr7($request);
-				$content = ob_get_clean();
-				$psr7->respond(new Response(200, [], $content));
-			}
-			else {*/
-			$response = App::run();
-			$psr7->respond($response);
-			//}
-			$session->close();
-		} catch (Throwable $e) {
-			// In case of any exceptions in the application code, you should handle
-			// them and inform the client about the presence of a server error.
-			//
-			// Reply by the 500 Internal Server Error response
-			$psr7->respond(new Response(500, [], 'Something Went Wrong!'));
+            /*$get = $request->getQueryParams();
+            if (isset($get['_tracy_bar'])) {
+              $_GET = $get;
+              ob_start();
+              Debugger::dispatchPsr7($request);
+              $content = ob_get_clean();
+              $psr7->respond(new Response(200, [], $content));
+            }
+            else {*/
+            $response = App::run();
+            $psr7->respond($response);
+            //}
+            $session->close();
+        } catch (RouteNotFoundException $e) {
+            if (in_array('application/json', getAcceptTypes($request))) {
+                $psr7->respond(
+                  new Response(
+                    404, ['Content-Type' => 'application/json'], json_encode(
+                         new ErrorDto(
+                           'Route not found', detail: $e->getMessage(), type: ErrorType::NOT_FOUND, exception: $e
+                         )
+                       )
+                  )
+                );
+            }
+            else {
+                // TODO: Handle production mode
+                $psr7->respond(new Response(404, [], $e->getMessage()));
+            }
+        } catch (Throwable $e) {
+            if (in_array('application/json', getAcceptTypes($request))) {
+                $psr7->respond(
+                  new Response(
+                    500,
+                    ['Content-Type' => 'application/json'],
+                    json_encode(new ErrorDto('Something Went wrong!', detail: $e->getMessage(), exception: $e))
+                  )
+                );
+            }
+            else {
+                // TODO: Handle production mode
+                ob_start();
+                Debugger::getBlueScreen()->render($e);
+                $blueScreen = ob_get_clean();
 
-			// Additionally, we can inform the RoadRunner that the processing
-			// of the request failed.
-			$psr7->getWorker()->error((string) $e);
-			$logger->exception($e);
-		}
-	}
+                $psr7->respond(new Response(500, [], $blueScreen));
+            }
+
+            // Additionally, we can inform the RoadRunner that the processing
+            // of the request failed.
+            $psr7->getWorker()->error((string) $e);
+            $logger->exception($e);
+        }
+    }
+}
+
+/**
+ * @param  ServerRequestInterface  $request
+ * @return string[]
+ */
+function getAcceptTypes(ServerRequestInterface $request) : array {
+    $types = [];
+    foreach ($request->getHeader('Accept') as $value) {
+        $types[] = strtolower(trim(explode(';', $value, 2)[0]));
+    }
+    return $types;
 }

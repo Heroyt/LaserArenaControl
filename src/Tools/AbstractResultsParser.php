48e1552fe4d70d7e741be2fca1553cd80e7eccf2
Tomáš Vojík <vojik@wboy.cz>
2024-05-04 14:34:21 +0200

    :poop: Fixing import

--- src/Tools/AbstractResultsParser.php
+++ src/Tools/AbstractResultsParser.php
@@ -21,98 +21,99 @@ use Lsr\Exceptions\FileException;
 abstract class AbstractResultsParser implements ResultsParserInterface
 {
 
-	protected array  $matches      = [];
-	protected string $fileName     = '';
-	protected string $fileContents = '';
+    protected array $matches = [];
+    protected string $fileName = '';
+    protected string $fileContents = '';
 
-	/**
-	 * @param string $fileName
-	 *
-	 * @throws FileException
-	 */
-	public function __construct(
-		protected readonly PlayerProvider $playerProvider,
-	) {
-	}
+    /**
+     * @param  string  $fileName
+     *
+     * @throws FileException
+     */
+    public function __construct(
+      protected readonly PlayerProvider $playerProvider,
+    ) {}
 
-	/**
-	 * @return iterable<string>
-	 */
-	public function getFileLines(): iterable {
-		$separator = "\r\n";
-		/** @var string|false $line */
-		$line = strtok($this->getFileContents(), $separator);
-		while ($line !== false) {
-			yield $line;
-			$line = strtok($separator);
-		}
-	}
+    /**
+     * @return iterable<string>
+     */
+    public function getFileLines() : iterable {
+        $separator = "\r\n";
+        /** @var string|false $line */
+        $line = strtok($this->getFileContents(), $separator);
+        while ($line !== false) {
+            yield $line;
+            $line = strtok($separator);
+        }
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getFileContents(): string {
-		return $this->fileContents;
-	}
+    /**
+     * @return string
+     */
+    public function getFileContents() : string {
+        return $this->fileContents;
+    }
 
-	/**
-	 * @param string $pattern
-	 *
-	 * @return string[][]
-	 */
-	public function matchAll(string $pattern): array {
-		if (isset($this->matches[$pattern])) {
-			return $this->matches[$pattern];
-		}
-		preg_match_all($pattern, $this->getFileContents(), $matches);
-		$this->matches[$pattern] = $matches;
-		return $matches;
-	}
+    /**
+     * @param  string  $pattern
+     *
+     * @return string[][]
+     */
+    public function matchAll(string $pattern) : array {
+        if (isset($this->matches[$pattern])) {
+            return $this->matches[$pattern];
+        }
+        preg_match_all($pattern, $this->getFileContents(), $matches);
+        $this->matches[$pattern] = $matches;
+        return $matches;
+    }
 
-	/**
-	 * @param string $fileName
-	 *
-	 * @return $this
-	 * @throws FileException
-	 */
-	public function setFile(string $fileName): static {
-		if (!file_exists($fileName) || !is_readable($fileName)) {
-			throw new FileException('File "' . $fileName . '" does not exist or is not readable');
-		}
+    /**
+     * @param  string  $fileName
+     *
+     * @return $this
+     * @throws FileException
+     */
+    public function setFile(string $fileName) : static {
+        if (!file_exists($fileName) || !is_readable($fileName)) {
+            throw new FileException('File "'.$fileName.'" does not exist or is not readable');
+        }
 
-		$this->fileName = $fileName;
+        $this->fileName = $fileName;
 
-		$contents = file_get_contents($this->fileName);
-		if ($contents === false) {
-			throw new FileException('File "' . $this->fileName . '" read failed');
-		}
-		$this->fileContents = mb_convert_encoding($contents, 'UTF-8');
-		return $this;
-	}
+        $contents = file_get_contents($this->fileName);
+        if ($contents === false) {
+            throw new FileException('File "'.$this->fileName.'" read failed');
+        }
+        $this->fileContents = mb_convert_encoding($contents, 'UTF-8');
+        $this->matches = [];
+        return $this;
+    }
 
-	/**
-	 * @param string $fileContents
-	 *
-	 * @return $this
-	 */
-	public function setContents(string $fileContents): static {
-		$this->fileContents = $fileContents;
-		return $this;
-	}
+    /**
+     * @param  string  $fileContents
+     *
+     * @return $this
+     */
+    public function setContents(string $fileContents) : static {
+        $this->fileContents = $fileContents;
+        $this->matches = [];
+        return $this;
+    }
 
-	/**
-	 * @param G $game
-	 * @param array<string, mixed> $meta
-	 *
-	 * @return void
-	 */
-	protected function processExtensions(Game $game, array $meta): void {
-		$extensions = App::getContainer()->findByType(ResultParserExtensionInterface::class);
-		foreach ($extensions as $extensionName) {
-			/** @var ResultParserExtensionInterface $extensions */
-			$extensions = App::getService($extensionName);
-			$extensions->parse($game, $meta, $this);
-		}
-	}
+    /**
+     * @param  G  $game
+     * @param  array<string, mixed>  $meta
+     *
+     * @return void
+     */
+    protected function processExtensions(Game $game, array $meta) : void {
+        $extensions = App::getContainer()->findByType(ResultParserExtensionInterface::class);
+        foreach ($extensions as $extensionName) {
+            /** @var ResultParserExtensionInterface $extensions */
+            $extensions = App::getService($extensionName);
+            $extensions->parse($game, $meta, $this);
+        }
+    }
 
 }

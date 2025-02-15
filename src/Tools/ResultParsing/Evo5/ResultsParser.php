913478704a820546b3a0779b95d1c4da064ecb9d
Tomáš Vojík <vojik@wboy.cz>
2025-02-15 23:27:28 +0100

    :bug: Fixed result parsing

--- src/Tools/ResultParsing/Evo5/ResultsParser.php
+++ src/Tools/ResultParsing/Evo5/ResultsParser.php
@@ -7,6 +7,9 @@
 namespace App\Tools\ResultParsing\Evo5;
 
 use App\GameModels\Game\Evo5\Game;
+use App\Models\Auth\Player;
+use App\Models\GameGroup;
+use App\Models\MusicMode;
 use App\Tools\ResultParsing\WithExtensions;
 
 /**
@@ -17,4 +20,8 @@ use App\Tools\ResultParsing\WithExtensions;
 class ResultsParser extends \Lsr\Lg\Results\LaserMaxx\Evo5\ResultsParser
 {
     use WithExtensions;
+
+    public const string  MUSIC_CLASS = MusicMode::class;
+    public const string GAME_GROUP_CLASS = GameGroup::class;
+    public const string USER_CLASS = Player::class;
 }
--- src/Tools/ResultParsing/Evo6/ResultsParser.php
+++ src/Tools/ResultParsing/Evo6/ResultsParser.php
@@ -7,6 +7,9 @@
 namespace App\Tools\ResultParsing\Evo6;
 
 use App\GameModels\Game\Evo6\Game;
+use App\Models\Auth\Player;
+use App\Models\GameGroup;
+use App\Models\MusicMode;
 use App\Tools\ResultParsing\WithExtensions;
 
 /**
@@ -16,6 +19,9 @@ use App\Tools\ResultParsing\WithExtensions;
  */
 class ResultsParser extends \Lsr\Lg\Results\LaserMaxx\Evo6\ResultsParser
 {
-
     use WithExtensions;
+
+    public const string  MUSIC_CLASS = MusicMode::class;
+    public const string GAME_GROUP_CLASS = GameGroup::class;
+    public const string USER_CLASS = Player::class;
 }

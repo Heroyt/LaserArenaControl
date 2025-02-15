1a8b4f084c965c169d03e8e01d865fbf6ee68f48
Tomáš Vojík <vojik@wboy.cz>
2025-02-15 23:16:43 +0100

    :bug: Cron import all

--- src/Cron/ResultsImportJob.php
+++ src/Cron/ResultsImportJob.php
@@ -2,12 +2,12 @@
 
 namespace App\Cron;
 
+use App\Models\System;
 use App\Services\ImportService;
 use Lsr\Core\Requests\Dto\ErrorResponse;
 use Lsr\Logging\Logger;
 use Orisai\Scheduler\Job\Job;
 use Orisai\Scheduler\Job\JobLock;
-use RuntimeException;
 use Spiral\RoadRunner\Metrics\Metrics;
 use Throwable;
 
@@ -33,31 +33,36 @@ final readonly class ResultsImportJob implements Job
         $this->metrics->add('cron_job_started', 1, ['results_import']);
 
         $lock->refresh(30.0);
-
-        $this->metrics->add('import_planned', 1, ['cron']);
-        try {
-            $response = $this->importService->import(DEFAULT_RESULTS_DIR);
-        } catch (Throwable $e) {
-            $this->logger->exception($e);
-            $this->metrics->add('cron_job_error', 1, ['results_import']);
-            throw new RuntimeException('Error while running import', 0, $e);
-        }
-
-        if ($response instanceof ErrorResponse) {
-            $this->logger->error(
-              $response->title.(!empty($response->detail) ? ' '.$response->detail : ''),
-              $response->values ?? []
-            );
-            if (isset($response->exception)) {
-                $this->logger->exception($response->exception);
+        foreach (System::getActive() as $system) {
+            $resultsDir = $system->resultsDir;
+            if (!file_exists($resultsDir)) {
+                continue;
             }
-            $this->metrics->add('cron_job_error', 1, ['results_import']);
-            throw new RuntimeException($response->title, 0, $response->exception);
-        }
 
-        if ($response->imported) {
-            $this->logger->info('Imported '.$response->imported.'/'.$response->total.' results.');
+            $this->metrics->add('import_planned', 1, ['cron']);
+            try {
+                $response = $this->importService->import(DEFAULT_RESULTS_DIR);
+            } catch (Throwable $e) {
+                $this->logger->exception($e);
+                $this->metrics->add('cron_job_error', 1, ['results_import']);
+                continue;
+            }
+
+            if ($response instanceof ErrorResponse) {
+                $this->logger->error(
+                  $response->title.(!empty($response->detail) ? ' '.$response->detail : ''),
+                  $response->values ?? []
+                );
+                if (isset($response->exception)) {
+                    $this->logger->exception($response->exception);
+                }
+                $this->metrics->add('cron_job_error', 1, ['results_import']);
+                continue;
+            }
+            if ($response->imported) {
+                $this->logger->info('Imported '.$response->imported.'/'.$response->total.' results.');
+            }
+            $this->metrics->add('cron_job_ok', 1, ['results_import']);
         }
-        $this->metrics->add('cron_job_ok', 1, ['results_import']);
     }
 }
